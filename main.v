460ad74d50580b3e4a079c5d39bf9d2bfbfb164b
Sohan Shanbhag <127968647+sohanshanbhag1502@users.noreply.github.com>
2023-11-25 20:45:23 +0530

    Add files via upload

--- main.v
+++ main.v
@@ -1,3 +1,12 @@
+module demux(input wire sel, inp, output reg p0, p1);
+    always @ * begin
+        if (sel==0)
+            p0=inp;
+        else
+            p1=inp;
+    end
+endmodule
+
 module dflipflop(input wire clk, inp, reset, output wire out);
     reg dfout;
     always @(posedge clk) begin
@@ -9,11 +18,25 @@ module dflipflop(input wire clk, inp, reset, output wire out);
     assign out=dfout;
 endmodule
 
-module Sreg(input wire clk, sinp, reset, output wire [3:0]out, output wire sout);
+module siso(input wire clk, sinp, reset, output wire sout);
+    wire temp[2:0];
+    dflipflop df1(clk, sinp, reset, temp[0]);
+    dflipflop df2(clk, temp[0], reset, temp[1]);
+    dflipflop df3(clk, temp[1], reset, temp[2]);
+    dflipflop df4(clk, temp[2], reset, sout);
+endmodule
+
+module sipo(input wire clk, sinp, reset, output wire [3:0]out);
     dflipflop df1(clk, sinp, reset, out[0]);
     dflipflop df2(clk, out[0], reset, out[1]);
     dflipflop df3(clk, out[1], reset, out[2]);
     dflipflop df4(clk, out[2], reset, out[3]);
+endmodule
 
-    assign sout=out[3];
+module Sreg(input wire clk, sinp, resetsi, resetpo, choice, output wire [3:0]out, 
+output wire sout);
+    wire inp1, inp2;
+    demux d1(choice, sinp, inp1, inp2);
+    siso s1(clk, inp1, resetsi, sout);
+    sipo s2(clk, inp2, resetpo, out);
 endmodule
---
+++ tb5.v
@@ -0,0 +1,120 @@
+`timescale 1ns / 1ps
+
+module Sreg_tb();
+
+    reg tclk, tsinp, tresetsi, tresetpo, tchoice;
+    wire tsout;
+    wire [3:0]tpout;
+
+    Sreg reg1(.clk(tclk), .sinp(tsinp), .out(tpout), .sout(tsout), .resetsi(tresetsi), 
+    .resetpo(tresetpo), .choice(tchoice));
+
+    initial begin
+        $dumpfile("dump5.vcd");
+        $dumpvars(0, Sreg_tb);
+    end
+
+    initial begin
+        tclk=1'b0;
+        forever #5 tclk=~tclk;
+    end
+
+    initial begin
+        $monitor("clk=%d SerialInput=%d ParallelOutput=%b SerialOutput=%d", 
+        tclk, tsinp, tpout, tsout);
+    end
+
+    initial begin
+        tresetsi=1'b1;
+        tresetpo=1'b1;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b1;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b1;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b1;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b0;
+        tresetsi=1'b0;
+        tresetpo=1'b1;
+        tsinp=1'b0;
+        #10
+        tresetsi=1'b1;
+        tresetpo=1'b1;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b1;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b1;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b1;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b0;
+        #10
+        tchoice=1'b1;
+        tresetsi=1'b1;
+        tresetpo=1'b0;
+        tsinp=1'b0;
+        #10
+        tresetsi=1'b1;
+        tresetpo=1'b1;
+        #10
+        $finish;
+    end
+
+endmodule
